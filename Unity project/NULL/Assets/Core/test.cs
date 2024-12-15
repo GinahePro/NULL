660152f579d85a224a8350a25d2468292c315f9f
GinahePro <ginahe.pro@gmail.com>
2024-12-15 13:20:08 +0100

    test

--- Unity project/NULL/Assets/Core/test.cs
+++ Unity project/NULL/Assets/Core/test.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,26 @@ using UnityEngine.InputSystem;
 
 public class test : MonoBehaviour
 {
+    [SerializeField] PlayerInput input;
+    [SerializeField] Transform camTransform;
+
     // Start is called before the first frame update
     void Start()
     {
+        input.actionEvents[2].AddListener(onDelta);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void onDelta(InputAction.CallbackContext ctx)
+    {
+        Vector2 inputValue = ctx.ReadValue<Vector2>();
+        camTransform.Rotate(Vector3.up, inputValue.x/50,Space.World);
+        camTransform.Rotate(Vector3.left, inputValue.y / 50, Space.Self);
     }
 
     void OnGravity(InputValue value)
